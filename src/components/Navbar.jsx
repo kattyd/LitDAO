b225847c284ac19080f598e4ec5c847dc7df1d07
Katrina Dawap <kattydee3@gmail.com>
2025-06-24 15:38:35 +0100

    made mobile responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
-import { FaWallet } from "react-icons/fa";
+import { FaBars, FaTimes, FaWallet } from "react-icons/fa";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
-     const [walletConnected, setWalletConnected] = useState(false);
+        const [isOpen, setIsOpen] = useState(false);
+
+        const toggleMenu = () => setIsOpen(!isOpen);
+        const closeMenu = () => setIsOpen(false);
+
+
+        const [walletConnected, setWalletConnected] = useState(false);
         const [address, setAddress] = useState("");
     
         const connectWallet = async () => {
@@ -25,19 +31,26 @@ function Navbar() {
             <div className="logo">
                 <Link to="/">LitDAO</Link>
             </div>
-            <ul className="nav-links">
-                <li><Link to="/dashboard">dashboard</Link></li>
-                <li><Link to="/create">create proposal</Link></li>
-                <li><Link to="#about">about</Link></li>
-            </ul>
-            {!walletConnected ? (
-                                <button onClick={connectWallet} className="nav-btn">
-                                    <FaWallet style={{ marginRight: "8px" }} />
-                                    connect wallet
-                                </button>
-                            ) : (
-                                <p>connected: {address.slice(0, 6)}...{address.slice(-4)}</p>
-                            )}
+            <div className={`nav-links ${isOpen ? "open" : ""}`}>
+                <ul>
+                    <li><Link to="/dashboard">dashboard</Link></li>
+                    <li><Link to="/create">create proposal</Link></li>
+                    <li><Link to="/about">about</Link></li>
+                </ul>
+            </div>
+                <div className="navbtn">
+                    {!walletConnected ? (
+                                    <button onClick={connectWallet} className="nav-btn">
+                                        <FaWallet style={{ marginRight: "8px" }} />
+                                        connect wallet
+                                    </button>
+                                ) : (
+                                    <p>connected: {address.slice(0, 6)}...{address.slice(-4)}</p>
+                                )}
+            </div>
+            <div className="hamburger" onClick={toggleMenu}>
+                {isOpen ? <FaTimes /> : <FaBars />}
+            </div>
         </nav>
     );
 }
